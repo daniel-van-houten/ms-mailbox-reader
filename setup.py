6a41b92561c802c8a0d5a33eca19fa376b31d50b
Daniel Van Houten <daniel.vanhouten87@gmail.com>
2023-05-11 10:08:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ms_mailbox_reader",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "pywin32"
